20d70b0fa863111279e4696e24a4e23e82932327
NadaAEbrahim <alinada303@yahoo.com>
2018-06-03 18:22:52 +0200

    AGAZA

---
+++ Room1/js/mine.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+ var navOffset = $("nav").offset().top;
+
+ $("nav").wrap('<div class="nav-placeholder" style="height:auto;"></div>');
+ $(".nav-placeholder").height($("nav").outerHeight());
+
+ $(window).scroll(function(){
+    var scrollPos = $(window).scrollTop();
+
+    if(scrollPos>=navOffset){
+        $("nav").addClass("fixed");
+    }else {
+        $("nav").removeClass("fixed");
+    }
+    
+   });
+
+
+});
